9e497bee036ec6a8139c69309cfbfb1430fab465
Bernardo-Junior <bernardojunior540@gmail.com>
2021-02-20 12:30:11 -0400

    style: styling component

--- src/presentation/pages/Login/index.tsx
+++ src/presentation/pages/Login/index.tsx
@@ -1,15 +1,105 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-community/google-signin';
 
 import {  
   Container,
-  Txt
+  Txt,
+  BtnGoogle,
+  Statusbar,
+  ImgUser,
+  TxtNameUser,
+  ViewInfo,
+  TxtWelcome,
+  BtnLogOut,
+  TxtBtnLogOut
 } from './styles';
 
+import { IUserLogin } from '../../../data/protocols/LoginGoogle';
+
 const Login: React.FC = () => {
+  const [isSigninInProgress, setisSigninInprogress] = useState<boolean>(false);
+  const [infoUser, setInfoUser] = useState<IUserLogin | null>(null);
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+      webClientId: '840685815549-p9vi2lqjbk9lm56meq4rr4tncrgjgpkh.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+      //hostedDomain: '', // specifies a hosted domain restriction
+      //loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+      forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+      //accountName: '', // [Android] specifies an account name on the device that should be used
+      //iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+    });
+  },[])
+
+  const _signIn = async() => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      setInfoUser(userInfo);
+      setisSigninInprogress(true);
+      // setState({ userInfo });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+      console.log(error);
+    }
+  }
+
+  const signOut = async () => {
+    try {
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+      setInfoUser(null) // Remember to remove the user from your app's state as well
+      setisSigninInprogress(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <Container>
-      <Txt>Login</Txt>
-    </Container>
+    <>
+      <Statusbar backgroundColor={'#000000'} barStyle={"light-content"} />
+      <Container>
+        {!isSigninInProgress &&
+        <>
+          <Txt>Login com Google</Txt>
+          <BtnGoogle
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={_signIn}
+          disabled={isSigninInProgress} />
+        </>
+        }
+        {infoUser &&
+          <>
+            <ViewInfo>
+              <TxtWelcome>Bem vindo</TxtWelcome>
+              <ImgUser 
+                source={{ uri: `${infoUser.user?.photo}` }}
+                resizeMode="stretch"
+              />
+              <TxtNameUser>{infoUser.user?.name}</TxtNameUser>
+
+              <BtnLogOut onPress={() => {signOut()}}>
+                <TxtBtnLogOut>deslogar</TxtBtnLogOut>
+              </BtnLogOut>
+            </ViewInfo>
+          </>
+        }
+      </Container>
+    </>
   )
 }
 
--- src/presentation/pages/Login/styles.ts
+++ src/presentation/pages/Login/styles.ts
@@ -1,10 +1,73 @@
 import styled from 'styled-components/native';
 
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-community/google-signin';
+
+import {
+  wp,
+  hp
+} from '../../../utils/responsivity';
+
 export const Container = styled.View`
   flex: 1;
   background-color: #FFFFFF;
+  align-items: center;
 `;
 
 export const Txt = styled.Text`
   color: #000000;
+  align-self: center;
+  font-size: ${hp('3%')}px;
+  padding: ${hp('5%')}px;
 `;
+
+export const BtnGoogle = styled(GoogleSigninButton)`
+  width: ${wp('60%')}px;
+  height: ${hp('7%')}px;
+`;
+
+export const Statusbar = styled.StatusBar``;
+
+export const ViewInfo = styled.View`
+  flex: 1;
+  margin-top: ${hp('15%')}px;
+  align-items: center;
+`;
+
+export const ImgUser = styled.Image`
+  width: ${wp('30%')}px;
+  height: ${hp('15%')}px;
+`;
+
+
+export const TxtNameUser = styled.Text`
+  margin-top: ${hp('5%')}px;
+  text-align: center;
+  color: blue;
+  font-size: ${hp('3%')}px;
+  font-weight: bold;
+`;
+
+export const TxtWelcome = styled.Text`
+  font-size: ${hp('6%')}px;
+  color: #FFFFFF;
+  margin-bottom: ${hp('5%')}px;
+`;
+
+export const BtnLogOut = styled.TouchableOpacity`
+  width: ${wp('60%')}px;
+  height: ${hp('7%')}px;
+  background-color: blue;
+  justify-content: center;
+  align-items: center;
+  margin-top: ${hp('10%')}px;
+  border-radius: ${hp('1%')}px;
+`;
+
+export const TxtBtnLogOut = styled.Text`
+  font-weight: bold;
+`;
+
